14c826641607053e08069022c246e4471c7463f7
dfgiordano <federico.giordano90@gmail.com>
2021-07-12 17:27:24 +0200

    home terminata

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,42 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../style/style.css">
         <title>I miei primi passi con Laravel</title>
     </head>
     <body>
-        <h1>I miei primi passi con Laravel</h1>
+        <header>
+            <div class="title">
+                <h1>I miei primi passi con Laravel</h1>
+            </div>
+            <hr>
+            <div class="menu_nav">
+                <ul>
+                    @foreach ($links as $link)
+                    <li> <a href="#"> {{ $link }} </a> </li> 
+                    @endforeach 
+                </ul>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                @foreach ($mangas as $key => $manga)
+                <div class="item">
+                    <div>
+                        <img src="{{ $manga['cover'] }}"alt="###">
+                    </div>
+                    <h3> <a href="#"> {{ $manga['title']}} </a> </h3>
+                    <h5> <a href="#"> {{ $manga['year']}} </a> </h5>
+                </div>
+                @endforeach
+            </div>
+        </main>
+        <footer>
+            <ul>
+                @foreach ($footer as $link)
+                <li> <a href="#"> {{ $link }} </a> </li> 
+                @endforeach 
+            </ul>
+        </footer>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function PHPSTORM_META\map;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +16,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        "links" => [
+            "home" => "home",
+            "contacts" => "contatti",
+            "collection" => "collezioni",
+            "login" => "login",
+            "register" => "registrati"
+        ],
+        "mangas" => [
+            "slamdunk" => [
+                "cover" => "https://www.animeclick.it/immagini/manga/Slam_Dunk/cover/Slam_Dunk-cover.jpg",
+                "title" => "Slamdunk",
+                "year" => "1993"
+            ],
+            "dragonball" => [
+                "cover" => "https://images-na.ssl-images-amazon.com/images/I/81yC-HFDoJL.jpg",
+                "title" => "Dragonball",
+                "year" => "1984"
+            ],
+            "cityhunter" => [
+                "cover" => "https://images-na.ssl-images-amazon.com/images/I/71DVBvAx7NL.jpg",
+                "title" => "City Hunter",
+                "year" => "1985"
+            ],
+        ],
+        "footer" => [
+            "prices" => "Vai al catalogo prezzi",
+            "collections" => "Nuove collezioni",
+            "newsletter" => "Iscriviti alla newsletter"
+        ]
+    ];
+    return view('home',$data) ;
+});
+
+Route::get('/contatti', function () {
+    return view('contatti') ;
 });
